53351312329e386f9fee9a919000dd1fd5ffa91d
Maureenkamau03 <mkaymaureen@gmail.com>
2022-07-17 12:30:08 +0300

    declaration of variables

---
+++ assets/js/app.js
@@ -0,0 +1,62 @@
+// variables, arrays and objects declaration
+let shed_a = 0;
+let shed_b = 0;
+let shed_c = 0;
+let shed_d = 0;
+let total = 0;
+let price = 45;
+
+let months = [
+	{
+		name: "January",
+		days: 31,
+	},
+	{
+		name: "February",
+		days: 29,
+	},
+	{
+		name: "March",
+		days: 31,
+	},
+	{
+		name: "April",
+		days: 30,
+	},
+	{
+		name: "May",
+		days: 31,
+	},
+	{
+		name: "June",
+		days: 30,
+	},
+	{
+		name: "July",
+		days: 31,
+	},
+	{
+		name: "August",
+		days: 31,
+	},
+	{
+		name: "September",
+		days: 30,
+	},
+	{
+		name: "October",
+		days: 31,
+	},
+	{
+		name: "November",
+		days: 30,
+	},
+	{
+		name: "December",
+		days: 31,
+	},
+	{
+		name: "July",
+		days: 31,
+	},
+];
