ecd5d46c04a9a6df8044bb80bbae344697a29adc
Maureenkamau03 <mkaymaureen@gmail.com>
2022-07-17 21:40:38 +0300

    calculating yearly income

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,13 +1,14 @@
-// variables, arrays and objects declaration
+let sheds = document.getElementById("form");
 let shed_a = 0;
 let shed_b = 0;
 let shed_c = 0;
 let shed_d = 0;
 let total = 0;
 let price = 45;
-const sheds = document.getElementById("form");
 
-let months = [
+// An object to hold months of an year and their days.
+
+const months = [
 	{
 		name: "January",
 		days: 31,
@@ -36,104 +37,81 @@ let months = [
 		name: "July",
 		days: 31,
 	},
-	{
-		name: "August",
-		days: 31,
-	},
-	{
-		name: "September",
-		days: 30,
-	},
-	{
-		name: "October",
-		days: 31,
-	},
-	{
-		name: "November",
-		days: 30,
-	},
-	{
-		name: "December",
-		days: 31,
-	},
-	{
-		name: "July",
-		days: 31,
-	},
 ];
 
-// functions
-// total productions function which is called upon clickicking the form to display production per shed resets the values hides the form afterwards
-const totalProduction = (event) => {
+// Functions.
+const handleShedsFormSubmit = (event) => {
 	// To prevent refreshing the page whenever the button is clicked.
 	event.preventDefault();
-
+	// Get the values from the form.
+	// shed a.
 	shed_a = parseInt(document.getElementById("shed_a").value);
+	// shed b.
 	shed_b = parseInt(document.getElementById("shed_b").value);
+	// shed c.
 	shed_c = parseInt(document.getElementById("shed_c").value);
+	// shed d.
 	shed_d = parseInt(document.getElementById("shed_d").value);
 
 	// error handling.
 	// checking if value of each shed is set.
-	if (!shed_a) {
-		document.getElementById("shed_a_error").innerHTML =
-			"Value for Shed A required";
-		return;
-	}
-	// else {
+	// if (!shed_a) {
+	// 	document.getElementById("shed_a_error").innerHTML =
+	// 		"Value for Shed A required";
+	// 	return;
+	// } else {
 	// 	document.getElementById("shed_a_error").innerHTML = "";
 	// }
 
-	if (!shed_b) {
-		document.getElementById("shed_b_error").innerHTML =
-			"Value for Shed B required";
-		return;
-	}
-	// else {
+	// if (!shed_b) {
+	// 	document.getElementById("shed_b_error").innerHTML =
+	// 		"Value for Shed B required";
+	// 	return;
+	// } else {
 	// 	document.getElementById("shed_b_error").innerHTML = "";
 	// }
 
-	if (!shed_c) {
-		document.getElementById("shed_c_error").innerHTML =
-			"Value for Shed C required";
-		return;
-	}
-	//  else {
+	// if (!shed_c) {
+	// 	document.getElementById("shed_c_error").innerHTML =
+	// 		"Value for Shed C required";
+	// 	return;
+	// } else {
 	// 	document.getElementById("shed_c_error").innerHTML = "";
 	// }
 
-	if (!shed_d) {
-		document.getElementById("shed_d_error").innerHTML =
-			"Value for Shed D required";
-		return;
-	}
-	// else {
+	// if (!shed_d) {
+	// 	document.getElementById("shed_d_error").innerHTML =
+	// 		"Value for Shed D required";
+	// 	return;
+	// } else {
 	// 	document.getElementById("shed_d_error").innerHTML = "";
 	// }
 
+	// getting the totals.
 	total = shed_a + shed_b + shed_c + shed_d;
-	// after getting the inputs we need to reset the values
-	document.getElementById(shed_a).value = "";
-	document.getElementById(shed_a).value = "";
-	document.getElementById(shed_a).value = "";
-	document.getElementById(shed_a).value = "";
 
-	// Hiding the form to display reports
+	// reset the values.
+	document.getElementById("shed_a").value = "";
+	document.getElementById("shed_b").value = "";
+	document.getElementById("shed_c").value = "";
+	document.getElementById("shed_d").value = "";
+
+	// hide the form.
 	document.getElementById("form").style.display = "none";
 
-	// show daily report
+	// show the user the report.
 	document.getElementById(
 		"shed_a_report"
-	).innerText = `Your production in Shed A is ${shed_a}`;
+	).innerText = `Value for Shed A is ${shed_a}`;
 	document.getElementById(
 		"shed_b_report"
-	).innerText = `Your production in Shed B is ${shed_b}`;
+	).innerText = `Value for Shed B is ${shed_b}`;
 	document.getElementById(
 		"shed_c_report"
-	).innerText = `Your production in Shed C is ${shed_c}`;
+	).innerText = `Value for Shed C is ${shed_c}`;
 	document.getElementById(
-		"shed_D_report"
-	).innerText = `Your production in Shed D is ${shed_d}`;
+		"shed_d_report"
+	).innerText = `Value for Shed D is ${shed_d}`;
 
 	document.getElementById(
 		"total_shed_report"
@@ -144,6 +122,7 @@ const totalProduction = (event) => {
 };
 
 function totalIncome(price, time) {
+	// total income in time...changed to string
 	let result = (total * time * price).toLocaleString();
 	return result;
 }
@@ -151,26 +130,34 @@ function totalIncome(price, time) {
 // Event listeners.
 
 // When form is clicked.
+sheds.addEventListener("submit", handleShedsFormSubmit);
 
-sheds.addEventListener("submit", totalProduction);
-
-// report action buttons
+// report action buttons.
 const get_weekly_income = document.getElementById("get_weekly_income");
 const get_total_yearly_income = document.getElementById(
 	"get_total_yearly_income"
 );
 const get_yearly_income = document.getElementById("get_yearly_income");
 
-// another event listener
-// when the total yearly button is clicked
+// When weekly button is clicked.
+get_weekly_income.addEventListener("click", () => {
+	let result = totalIncome(price, 7);
+	document.getElementById("list-monthly-incomes").innerHTML = ""; // remove any monthly incomes listed.
+	return (document.getElementById(
+		"income-report"
+	).innerText = `Your weekly income is KES ${result}`);
+});
+
+// when total yearly button is clicked.
 get_total_yearly_income.addEventListener("click", () => {
 	let result = totalIncome(price, 366);
+	document.getElementById("list-monthly-incomes").innerHTML = ""; // remove any monthly incomes listed.
 	return (document.getElementById(
 		"income-report"
 	).innerText = `Your annual income is KES ${result}`);
 });
 
-// when the yearly button is clicked
+// when yearly button is clicked.
 get_yearly_income.addEventListener("click", () => {
 	document.getElementById("income-report").innerHTML = ""; // remove any income listed
 	// get the unordered list item.
@@ -184,3 +171,7 @@ get_yearly_income.addEventListener("click", () => {
 	}
 	return;
 });
+
+function changePrice(newPrice) {
+	price = newPrice;
+}
